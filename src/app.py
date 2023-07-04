164c9934a579b728bfc735904364931deb91c838
“Facu <facuun.vicente@gmail.com>
2023-07-04 16:07:36 -0300

    Add endpoints to create and get users

--- src/app.py
+++ src/app.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from passlib.context import CryptContext
+from databases import Database
+from src.models.User import User
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Configuración de base de datos
+database = Database('postgresql://root:admin@postgres/users')
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
@@ -9,3 +16,32 @@ def read_root():
         "message": "Hello world"
     }
 
+@app.post("/users")
+async def create_user(User: User):
+    query = "INSERT INTO users (username, password) VALUES (:username, :password)"
+    values = {"username": User.username, "password": pwd_context.hash(User.password)}
+    try:
+        await database.connect()
+        await database.execute(query=query, values=values)
+        await database.disconnect()
+        return {"message": "User created successfully"}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    query = "SELECT * FROM users WHERE id = :user_id"
+    values = {"user_id": user_id}
+    try:
+        await database.connect()
+        result = await database.fetch_one(query=query, values=values)
+        if result:
+            user = User(id=result["id"], username=result["username"], password=result["password"])
+            await database.disconnect()
+            return user
+        else:
+            await database.disconnect()
+            raise HTTPException(status_code=404, detail="User not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
